9dcd963c2a74b21c67e8183e89179fb83e1e1127
AnishDaswani <akhem2010@gmail.com>
2025-09-27 16:19:40 -0400

    :)

--- soul-d-out/character_body_2d.gd
+++ soul-d-out/character_body_2d.gd
@@ -11,7 +11,7 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -22,4 +22,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+func _process(_delta):
+	if Input.is_action_pressed("right"):
+		_animated_sprite.play("walk")
+	else:
+		_animated_sprite.stop()
 	move_and_slide()
