5c2132f7f31933280c466d8612f74ba9b446ff1e
Portable Watermelons <sriharshaplayschess@gmail.com>
2025-09-27 16:45:03 -0400

    added player flipping directions

--- soul-d-out/character_body_2d.gd
+++ soul-d-out/character_body_2d.gd
@@ -17,9 +17,16 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
+	
 	if direction:
+		if velocity.x < 0:
+			$Sprite2D.flip_h = true
+		elif velocity.x > 0:
+			$Sprite2D.flip_h = false
+		print(direction, ":", velocity.x)
 		velocity.x = direction * SPEED
+		print(direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
